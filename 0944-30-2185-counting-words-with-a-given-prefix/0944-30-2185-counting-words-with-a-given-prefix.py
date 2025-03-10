ce7fe5e9949815cc7abdd47ae3564efa8239435d
joshSSL <mr.tashiev@gmail.com>
2025-03-10 15:21:27 +0600

    Delete 0944-30-2185-counting-words-with-a-given-prefix directory

--- 0944-30-2185-counting-words-with-a-given-prefix/0944-30-2185-counting-words-with-a-given-prefix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def prefixCount(self, words: List[str], pref: str) -> int:
-        count=0
-        l=len(pref)
-        for i in words:
-            if i[:l:]==pref:
-                count+=1
-        return count
-        
